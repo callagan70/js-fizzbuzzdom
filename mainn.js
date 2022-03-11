1be04f7cd6dc88f463722f3ac28e37d8a91ca4b0
Angelo Melotti <96485963+callagan70@users.noreply.github.com>
2022-03-11 16:57:04 +0000

    Passo alle condizioni

--- mainn.js
+++ mainn.js
@@ -3,6 +3,6 @@
 
 for (var n = 1; n <= 100; n++){
 
-document.getElementById("view").innerHTML+= `<div class="numero"> ${n}</div>`
+document.getElementById("view").innerHTML+= `<div class="numero colore"> ${n}</div>`
 
 }
